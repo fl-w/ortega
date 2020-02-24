b6a43536f369d5cce7de3e9b683788c9571ded3b
Fol O <30976147+folws@users.noreply.github.com>
2020-02-24 21:31:57 +0000

    Update fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -19,5 +19,5 @@ function __ortega_prompt -a last_status
 
     set -q theme_hood_ornament; or set -l theme_hood_ornament "🢂"
 
-    printf "$theme_hood_ornament" ' '
+    echo -e "$theme_hood_ornament "
 end
